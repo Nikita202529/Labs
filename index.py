60131d5d700438e85ab012ec9e2c55db4c43bd94
Nikita202529 <nskarlov@mail.ru>
2022-10-27 00:53:18 +0300

    Create moving averages algorithm

--- index.py
+++ index.py
@@ -1,10 +1,7 @@
 from datetime import datetime
 
-# Читаем текстовый файл с данными
 f = open('textData.txt')
-# Сохраняем данные в переменную
 documentData = f.read()
-# Записываем данные в массив
 arrData = documentData.split( )
 
 dateArr = []
@@ -13,21 +10,16 @@ pressureDay = []
 
 i = 0
 while i < len(arrData):
-
     bool = 0
-
     if not(i % 2):
         str = arrData[i].replace('T', ' ')
         day = datetime.strptime(str, "%Y-%m-%d %H:%M:%S")
-
         j = 0
-
         while j < len(dateArr):
             if day.day == dateArr[j]:
                 bool = 1
                 break;
             j += 1
-
         if not(bool):
             dateArr.append(day.day)
     else:
@@ -39,8 +31,26 @@ while i < len(arrData):
     i += 1
 
 
+# print('Массив дат')
+# print(dateArr)
+# print('Массив давления')
+# print(pressureDay)
 
-print('Массив дат')
-print(dateArr)
-print('Массив давления')
-print(pressureDay)
+
+middle = []
+EMAmiddle = []
+
+for i in range(len(dateArr)):
+    sum = 0
+    for j in range(24):
+        sum += pressureDay[i][j]
+    middle.extend([round(sum/24, 1)])
+    if i < 1:
+        EMAmiddle.extend([round(sum/24, 1)])
+    elif i > 0:
+        EMAmiddle.extend([round(((sum / 24) * (2 / (1 + i)) + (EMAmiddle[i - 1] * (1 - (2 / (1 + i))))), 1)])
+
+print('Массив среднесуточного давления')
+print(middle)
+print('Массив скользящего среднего давления')
+print(EMAmiddle)
